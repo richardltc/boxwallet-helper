1bb85fadc47f500db0f9c08eb0b128218fd87d54
Richard <richard.mace@gmail.com>
2024-06-01 07:26:41 +0100

    Refactoring Git installation detection

--- src/git.zig
+++ src/git.zig
@@ -1,3 +1,9 @@
+const cmd = @import("command_utils.zig");
+
 pub const Git = struct {
-    fn IsInstalled() !bool {}
+    pub fn IsInstalled() !bool {
+        const git_installed = try cmd.runCommandCheckOutput("git", "usage: git [-v | --version]");
+
+        return git_installed;
+    }
 };

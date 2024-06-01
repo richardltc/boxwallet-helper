2787de3dc557be803dd1aeb396cbd7da26d39edd
Richard <richard.mace@gmail.com>
2024-06-01 07:49:57 +0100

    Added Git instructions and improved run logic

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,8 @@ const cmd = @import("command_utils.zig");
 const git = @import("git.zig");
 
 pub fn main() !void {
+    defer std.debug.print("{s} {s} finished!...\n", .{ app.app.name, app.app.version });
+
     const out = std.io.getStdOut().writer();
     const green = "\x1b[32m";
     const red = "\x1b[31m";
@@ -11,7 +13,6 @@ pub fn main() !void {
     const tick = "✔";
     const cross = "X";
     // const cross = "✖";
-
     try out.print("{s} {s} starting...\t\t\t\t\t[{s}{s}{s}]\n", .{ app.app.name, app.app.version, green, tick, reset });
 
     const git_installed = try git.Git.IsInstalled(); //cmd.runCommandCheckOutput("git", "usage: git [-v | --version]2");
@@ -19,7 +20,7 @@ pub fn main() !void {
         try out.print("Checking if Git is installed...\t\t\t\t\t\t[{s}{s}{s}]\n", .{ green, tick, reset });
     } else {
         try out.print("Checking if Git is installed...\t\t\t\t\t\t[{s}{s}{s}]\n", .{ red, cross, reset });
+        try out.print("Git is not installed, please run the command:\n\nsudo apt install git\n\n", .{});
+        return;
     }
-
-    try out.print("{s} {s} finished!...\n", .{ app.app.name, app.app.version });
 }
